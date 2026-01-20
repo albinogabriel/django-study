d86dd6e5089ea43b0dc04c734dfedaacf6b1370d
albinogabriel <albino.gabriel.p@gmail.com>
2026-01-20 20:18:27 -0300

    header

--- gaming/views.py
+++ gaming/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'gaming/home.html')
+    return render(request, 'gaming/pages/home.html')
